88c744dbaf5c7485f42d67eca1e0dd7a26c5b425
lwabish <wubw@pku.edu.cn>
2024-02-29 17:53:20 +0800

    perf: update

--- handlers/content/handler.go
+++ handlers/content/handler.go
@@ -21,26 +21,26 @@ type Handler struct {
 func (h *Handler) Publish(c *gin.Context) {
 	tc := &models.TypechoContent{}
 	if err := c.ShouldBindWith(tc, binding.JSON); err != nil {
-		c.JSON(http.StatusBadRequest, err)
+		c.JSON(http.StatusBadRequest, err.Error())
 		return
 	}
 
 	if tc.IsNewPost() {
 		tc.PostDefault()
 		if r := h.db.Create(tc); r.Error != nil || r.RowsAffected != 1 {
-			c.JSON(http.StatusInternalServerError, r.Error)
+			c.JSON(http.StatusInternalServerError, r.Error.Error())
 			return
 		}
 		c.JSON(http.StatusCreated, tc.Cid)
 	} else {
 		old := &models.TypechoContent{Cid: tc.Cid}
 		if r := h.db.First(old); r.Error != nil || r.RowsAffected != 1 {
-			c.JSON(http.StatusNotFound, r.Error)
+			c.JSON(http.StatusNotFound, r.Error.Error())
 			return
 		}
 		old.UpdatePost(tc)
 		if r := h.db.Save(old); r.Error != nil || r.RowsAffected != 1 {
-			c.JSON(http.StatusInternalServerError, r.Error)
+			c.JSON(http.StatusInternalServerError, r.Error.Error())
 		}
 		c.JSON(http.StatusOK, tc.Cid)
 	}
--- models/typecho_contents.go
+++ models/typecho_contents.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 // PostDefault 文章默认模板
 // 对于数据库无默认值，且默认值和go的零值不匹配的字段，需要重设默认值
 // 仅用于新blog
@@ -8,6 +10,8 @@ func (c *TypechoContent) PostDefault() {
 	c.AllowComment = "1"
 	c.AllowPing = "1"
 	c.AllowFeed = "1"
+	c.Created = int32(time.Now().Unix())
+	c.Modified = int32(time.Now().Unix())
 	// todo: wordCount created modified
 	// todo: markdown basic
 	// markdown wrapper
@@ -21,6 +25,7 @@ func (c *TypechoContent) UpdatePost(new *TypechoContent) {
 	c.Title = new.Title
 	c.Slug = new.Slug
 	c.Text = new.Text
+	c.Modified = int32(time.Now().Unix())
 }
 
 func (c *TypechoContent) IsNewPost() bool {
